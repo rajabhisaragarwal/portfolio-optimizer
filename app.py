6f14d5762a66b869494bede38cca9e76e33f7b14
Raj Abhisar Agarwal, CFA <rajabhisaragarwal1@gmail.com>
2026-04-21 22:26:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -201,7 +201,7 @@ if run_button:
     fig.add_trace(go.Scatter(
         x=[min_vol_volatility], y=[min_vol_return],
         mode="markers",
-        marker=dict(size=18, color="#3498DB", symbol="square", opacity = 0.9, line=dict(color="white", width=1.5)),
+        marker=dict(size=18, color="#3498DB", symbol="diamond", opacity = 0.9, line=dict(color="white", width=1.5)),
         hovertext=[f"<b>Min Volatility</b><br>Return: {min_vol_return:.2%}<br>"
                    f"Volatility: {min_vol_volatility:.2%}<br>Sharpe: {min_vol_sharpe:.4f}<br><br>"
                    f"Weights:<br>" + "<br>".join([f"{t}: {w:.2%}" for t, w in zip(tickers, min_vol_weights)])],
